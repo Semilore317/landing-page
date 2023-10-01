7bb48a4dcec4fc40eacd92eaaaad22b8ecc35a5b
Semilore317 <semi.banks.317@gmail.com>
2023-10-01 21:30:53 +0100

    The website is done, only small bug fixes with the nav and media queries for mobile to be done�

---
+++ main.js
@@ -0,0 +1,122 @@
+const featuresArrow = document.querySelector('#features-arrow');
+const companyArrow = document.querySelector('#company-arrow');
+
+const feature = document.querySelector('#features');
+const featureDropDown = document.querySelector('.features-dropdown');
+
+const company = document.querySelector('#company');
+const companyDropDown = document.querySelector('.company-dropdown');
+console.log(company, companyDropDown);
+
+function featuresAppear(){
+    featureDropDown.style.display = 'flex';
+    featureDropDown.style.transform = 'translateY(0)';
+    featureDropDown.style.animationName = 'appear';
+
+    //change the arrow
+    featuresArrow.src = "upload.png";
+}
+ 
+function disappear(){
+    featureDropDown.display = 'none';
+    companyDropDown.display = 'none';
+}
+
+
+function featuresDisappear(){
+    featureDropDown.style.display = 'none'
+    featureDropDown.style.transform = 'translateY(8vh)';
+    featureDropDown.style.animationName = 'disappear';
+    //change the arrow
+    featuresArrow.src = "down-arrow.png"
+}
+
+
+function stay() {
+    featureDropDown.style.display = 'flex';
+    featureDropDown.style.transform = 'translateY()';
+    featureDropDown.style.animationName = '';
+}
+
+function companyAppear(){
+    companyDropDown.style.display = 'flex';
+    companyDropDown.style.transform = 'translateY(0)';
+    companyDropDown.style.animationName = 'appear';
+    
+    //change the arrow
+    companyArrow.src = "upload.png";
+}
+
+function companyDisappear(){
+    companyDropDown.style.display = 'none'
+    companyDropDown.style.transform = 'translateY(8vh)';
+    companyDropDown.style.animationName = 'disappear';
+
+    //change the arrow
+    companyArrow.src = "down-arrow.png";
+}
+
+function companyStay() {
+    companyDropDown.style.display = 'flex';
+}
+
+//the code is still buggy
+//the dropdown menu is not working properly
+
+
+//maintain aspect ratio of the image
+const imageContainer = document.querySelector('.img-container');
+function aspectRatio(){
+    if (window.innerWidth >= 869) {
+    const height = imageContainer.offsetHeight;
+    console.log(`height = ${height}`);
+    const width = height * (9 / 10);
+    console.log(`width = ${width}`);
+    imageContainer.style.width = `${width}px`;
+    imageContainer.style.height = `${height}px`;
+    console.log('done');    
+    }
+    else{
+        console.log('the screen is large');
+    }
+}
+//add the event listener
+window.addEventListener('load', aspectRatio);
+window.addEventListener('resize', aspectRatio);
+
+//Media queries
+const imageToBeChanged = document.querySelector(".img-container img");
+//change the image src when the breakpoint is reached
+
+function changeImg() {
+    if (window.innerWidth <= 869) {
+        imageToBeChanged.src = "images/image-hero-mobile.png";
+
+        //delete line-break
+        const lineBreak = document.querySelector('.text-container h1');
+        lineBreak.innerHTML = 'Make remote work';
+
+        //Maintain aspect ratio
+        if (window.innerWidth <= 869) {
+            const width = imageContainer.offsetWidth;
+            console.log(`width = ${width}`);
+            const height = width * (4 / 3);
+            console.log(`height = ${height}`);
+            imageContainer.style.width = `${width}px`;
+            imageContainer.style.height = `${height}px`;
+            console.log('done');    
+            }
+            else{
+                console.log('the screen is large');
+            }
+    }else if(window.innerWidth >= 869){
+        imageToBeChanged.src = "images/image-hero-desktop.png"
+    }
+
+}
+window.addEventListener('DOMContentLoaded',changeImg)
+window.addEventListener('',changeImg)
+window.addEventListener('load', changeImg);
+window.addEventListener('resize', changeImg);
+
+
